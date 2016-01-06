0ea84e1bf73f7e1b4f90a996163c952cd3ec40f2
Chloe Kaian Lam <chloe.lam@telegraph.co.uk>
2016-01-06 15:15:53 +0000

    Styling for large breakpoint, investigated gulpfile for concatenating and minifying js files

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -23,33 +23,6 @@ myApp.controller('aboutController', [
 		});
 	}
 ]);
-
-'use strict';
-
-var myApp = angular.module('myApp.work', ['ngRoute']);
-
-myApp.config(['$routeProvider', 
-	function($routeProvider) {
-		$routeProvider.when('/work', {
-			templateUrl: 'views/work/work.html',
-			controller: 'workController'
-		});
-	}
-]);
-
-myApp.controller('workController', [
-	'$scope',
-	'getContentData',
-	// getContentData is a global function passed as a parameter 
-	function($scope, getContentData) {
-		
-		// Passing retrieveData function from services
-		getContentData.retrieveData().then(function(data) {
-			$scope.contentData = data;
-		});
-	}
-]);
-
 'use strict';
 
 var myApp = angular.module('myApp.contact', ['ngRoute']);
@@ -64,6 +37,31 @@ myApp.config(['$routeProvider',
 ]);
 
 myApp.controller('contactController', [
+	'$scope',
+	'getContentData',
+	// getContentData is a global function passed as a parameter 
+	function($scope, getContentData) {
+		
+		// Passing retrieveData function from services
+		getContentData.retrieveData().then(function(data) {
+			$scope.contentData = data;
+		});
+	}
+]);
+'use strict';
+
+var myApp = angular.module('myApp.work', ['ngRoute']);
+
+myApp.config(['$routeProvider', 
+	function($routeProvider) {
+		$routeProvider.when('/work', {
+			templateUrl: 'views/work/work.html',
+			controller: 'workController'
+		});
+	}
+]);
+
+myApp.controller('workController', [
 	'$scope',
 	'getContentData',
 	// getContentData is a global function passed as a parameter 
